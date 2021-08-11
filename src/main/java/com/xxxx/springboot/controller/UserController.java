b30d0ce38542619b619920ffe388049612fbae1f
keykeyshome <keykeyshome@126.com>
2021-08-11 20:48:15 +0800

    swagger2 添加

--- src/main/java/com/xxxx/springboot/controller/UserController.java
+++ src/main/java/com/xxxx/springboot/controller/UserController.java
@@ -2,17 +2,21 @@ package com.xxxx.springboot.controller;
 
 import com.xxxx.springboot.exceptions.ParamException;
 import com.xxxx.springboot.model.ResultInfo;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import com.xxxx.springboot.service.UserService;
 import com.xxxx.springboot.vo.User;
 
 @RestController
+@Api(tags = "用户管理模块")
 public class UserController {
     @Autowired
     UserService userService;
 
     @GetMapping("user/uname/{userName}")
+    @ApiOperation(value = "用户模块-根据用户名查询用户",notes = "参数在路径上")
     public User queryUserByUserName(@PathVariable String userName) {
         return userService.queryUserByUserName(userName);
     }
