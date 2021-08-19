3ea820c84156fbbe9b84115e2ac0f307f73cc6b3
keykeyshome <keykeyshome@126.com>
2021-08-20 01:16:31 +0800

    1.controller 参数 添加 @valid注解 2.实体类User属性添加 @NotBlank @Length 3.全局异常， 添加参数绑定异常 controller参数是json的 异常 MethodArgumentNotValidException
    controller参数是实体类的 异常 MethodArgumentNotValidException

--- src/main/java/com/xxxx/springboot/GlobalExceptionHandler.java
+++ src/main/java/com/xxxx/springboot/GlobalExceptionHandler.java
@@ -2,6 +2,8 @@ package com.xxxx.springboot;
 
 import com.xxxx.springboot.exceptions.ParamException;
 import com.xxxx.springboot.model.ResultInfo;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -24,6 +26,16 @@ public class GlobalExceptionHandler {
         return resultInfo;
     }
 
+    //参数绑定异常
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ResultInfo resoverBindException(MethodArgumentNotValidException e) {
+        ResultInfo resultInfo = new ResultInfo();
+        resultInfo.setCode(8888);
+        resultInfo.setMsg(e.getBindingResult().getFieldError().getDefaultMessage());
+
+        return resultInfo;
+    }
 
     @ExceptionHandler
     @ResponseBody
@@ -32,7 +44,7 @@ public class GlobalExceptionHandler {
         resultInfo.setCode(9999);
         resultInfo.setMsg("操作失败全局异常捕获");
 
-     //没有 @ExceptionHandler(value = ParamException.class) 之前在全局内部判断
+        //没有 @ExceptionHandler(value = ParamException.class) 之前在全局内部判断
         /*   if (e instanceof ParamException) {
             ParamException pe = (ParamException) e;
             resultInfo.setCode(pe.getCode());
--- src/main/java/com/xxxx/springboot/vo/User.java
+++ src/main/java/com/xxxx/springboot/vo/User.java
@@ -2,16 +2,21 @@ package com.xxxx.springboot.vo;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
 @ApiModel(description = "用户实体对象")
 public class User implements Serializable {
     @ApiModelProperty(value = "用户id主键")
     private Integer id;
+    @NotBlank(message = "用户名不可以为空")
     @ApiModelProperty(value = "用户名字")
     private String userName;
     @ApiModelProperty(value = "用户密码")
+    @NotBlank(message = "用户密码不可以为空")
+    @Length(min = 6, max = 10, message = "密码长度至少6位但不超过10位")
     private String userPwd;
 
     public Integer getId() {
